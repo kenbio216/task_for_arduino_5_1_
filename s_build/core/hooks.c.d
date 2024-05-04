e8f2569963ed28c078fee58b8492bed8192afeaf
kenbio216 <3498592827@qq.com>
2024-05-04 17:03:33 +0800

    验证成功

--- s_build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-d:\Works\task_arduino_5_1_\s_build\core\hooks.c.o: \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\hooks.c
--- s_build/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-d:\Works\task_arduino_5_1_\s_build\libraries\Wire\utility\twi.c.o: \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
- C:\Users\hm\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
