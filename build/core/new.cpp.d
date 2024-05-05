eb26c09f7941a86925fa93378b4705807874aa6e
Xu Yang <3498592827@qq.com>
2024-05-05 14:29:48 +0800

    Delete build directory

--- build/core/new.cpp.d
+++
@@ -1,4 +0,0 @@
-E:\win_code_git\task_for_arduino_5_1_\build\core\new.cpp.o: \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new.cpp \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new.h \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new
--- build/core/wiring.c.d
+++
@@ -1,6 +0,0 @@
-E:\win_code_git\task_for_arduino_5_1_\build\core\wiring.c.o: \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\wiring.c \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\wiring_private.h \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\Arduino.h \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\binary.h \
- C:\Users\xuyang\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h
