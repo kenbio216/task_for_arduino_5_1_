4ed2b8be313db5cd0e350cc8f6ba173db66259d4
hm <784980575@qq.com>
2024-05-14 00:01:52 +0800

    帽子改了舵机旋转角度和蜂鸣器鸣叫方式

--- task6_ard_uno_maozi/task6_ard_uno_maozi.ino
+++ task6_ard_uno_maozi/task6_ard_uno_maozi.ino
@@ -3,24 +3,10 @@
 Servo myservo;
 int pos = 0;
 
-const int ldrPin = A0;    // 定义光敏电阻的引脚
-const int servoPin = 9;   // 定义SG90的引脚
-const int beepPin = 7;    // 定义蜂鸣器引脚
-const int beepPinGND = 4; // 定义蜂鸣器引脚
-
-void sweep() // 让舵机旋转
-{
-    for (pos = 0; pos <= 90; pos += 1)
-    {
-        myservo.write(pos);
-        delay(15);
-    }
-    for (pos = 90; pos >= 0; pos -= 1)
-    {
-        myservo.write(pos);
-        delay(15);
-    }
-}
+const int ldrPin = A0;     // 定义光敏电阻的引脚
+const int servoPin = 9;    // 定义SG90的引脚
+const int beepPin = 7;     // 定义蜂鸣器引脚
+const int beepPinGND = 4;  // 定义蜂鸣器引脚
 
 void setup()
 {
@@ -39,15 +25,23 @@ void loop()
 {
     int ldrValue = analogRead(ldrPin); // 读取模拟口A0的值，存入变量中
     Serial.print("光敏电阻得到的数值 = ");
-    Serial.println(ldrValue); // 输出gmValue的值到串口监视器
+    Serial.println(ldrValue);          // 输出gmValue的值到串口监视器
     delay(500);
-    if (ldrValue < 700)
+    
+    if (ldrValue < 500)                //光敏电阻阈值
     {
-        digitalWrite(beepPin, HIGH); // 蜂鸣器工作
-        sweep();
+        myservo.write(0);              //强光时，左边舵机驱动帽檐到达侧边
+
+        digitalWrite(beepPin, HIGH);   //蜂鸣器工作
+        delay(500);
+        digitalWrite(beepPin, LOW); 
+        delay(500);
     }
+
     else
     {
-        digitalWrite(beepPin, LOW); // 蜂鸣器停止工作
+        digitalWrite(beepPin, LOW);    //蜂鸣器停止工作
+        myservo.write(90);             //弱光时，左边舵机驱动帽檐到达前面
+        delay(1000);
     }
 }
